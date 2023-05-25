cc9de7307fafc97f809d6022f3b0660506a9d467
Alferezz <104406305+Alferezz@users.noreply.github.com>
2023-05-25 13:10:10 -0600

    Carrusel

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// Script for navigation bar
+$(document).ready(function() {
+    $('.carousel').slick({
+      // Configuraciones del carrusel
+      dots: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+  });
+  
