188e540a13b64c612d2c7f82df2a7ba94afa3266
Alferezz <104406305+Alferezz@users.noreply.github.com>
2023-05-29 23:47:13 -0600

    Add files via upload

--- wishes.js
+++ wishes.js
@@ -1,11 +1,44 @@
-// Obtener el enlace de la wishlist por su ID
-var wishlistLink = document.getElementById('wishlist-link');
+let wishList = [];
 
-// Agregar un evento de clic al enlace
-wishlistLink.addEventListener('click', function(event) {
-  event.preventDefault(); // Evitar el comportamiento predeterminado del enlace
+function setup() 
+{
+    let products = document.querySelectorAll(".but");
+    for (let i = 0; i < products.length; i++)
+    {
+        products[i].onclick = function(e) {
+            addItem(e);
+        }
+    }
+}
 
-  // Aquí puedes realizar acciones adicionales, como mostrar una ventana emergente, redirigir a una página de wishlist, etc.
-  // Por ejemplo, mostrar un mensaje en la consola
-  console.log('¡Has hecho clic en la wishlist!');
-});
+function addItem (e) {
+    let productId = e.target.getAttribute("id");
+    if(!wishList.find(element => element === productId)){
+        let productDiv = document.getElementById("product" + productId);
+
+        let wishDiv = document.createElement("div");
+        wishDiv.setAttribute("id", "wish" + productId);
+        wishDiv.setAttribute("class", "product");
+        wishDiv.setAttribute("style", "margin-bottom: 10px;")
+        wishDiv.innerHTML += productDiv.innerHTML;
+        let removeBtn = document.createElement("input");
+        removeBtn.setAttribute("id", "remove" + productId);
+        removeBtn.setAttribute("type", "button");
+        removeBtn.setAttribute("value", "Remove");
+        // removeBtn.setAttribute("class", "removebut");
+        removeBtn.onclick = () => removeItem(productId);
+        wishDiv.appendChild(removeBtn);
+
+        let aside = document.getElementById("wishlist");
+        aside.appendChild(wishDiv);
+
+        wishList.push(productId);
+    }
+}
+
+function removeItem(productId) {
+    document.getElementById("wish" + productId).remove();
+    wishList = wishList.filter(element => element !== productId)
+}
+
+window.addEventListener("load", setup);
