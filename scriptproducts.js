2b24ae28d2dc7ae2d71324f86ae5ab648cb4d3f3
BrendaAJ06 <125322847+BrendaAJ06@users.noreply.github.com>
2023-05-30 21:44:38 -0600

    Add files via upload

--- scriptproducts.js
+++ scriptproducts.js
@@ -12,10 +12,10 @@ http.onload = function(){
                <p class="title">${item.producto}</p>
                <p class="description">${item.descripcion}</p>
                <p class="price">
-                  <span>${item.precio}</span>
-                  <span>€</span>
+                  <span>$ ${item.precio}</span>
+                  <span>pesos</span>
                </p>
-               <p class="cart">Add to cart <i class="bx bx-cart-alt"></i></p>
+               <a href="sproduct.html"><i class="cart">Add to cart <i class="bx bx-cart-alt"></i></a>
             </div>
          `;
       }
